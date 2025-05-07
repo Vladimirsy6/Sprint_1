f4fa1c3a4e05aad4c1de42d2285d4e5f49e9f0f6
Vladimirsky <vladimirlatch@yandex.ru>
2025-05-07 15:15:59 +0300

    Внес исправления по комментариям от ревьюера

--- task_4.py
+++ task_4.py
@@ -1,14 +1,11 @@
 new_tasks = ['task_001', 'task_011', 'task_007', 'task_015', 'task_005']
 completed_tasks = ['task_002', 'task_012', 'task_006']
 
-# Перенос task_005 в список выполненных задач
-completed_task = new_tasks.pop(new_tasks.index('task_005'))
-completed_tasks.append(completed_task)
-print(f'Задача {completed_task} перенесена в список выполненных.')
+# Перенос task_005 в список выполненных задач (в одно действие)
+completed_tasks.append(new_tasks.pop(new_tasks.index('task_005')))
 
 # Удаление task_007 из списка новых задач
 new_tasks.remove('task_007')
-print('Задача task_007 удалена из списка новых задач (ожидается доработка требований).')
 
 # Вывод последней задачи из new_tasks
 print(f'Последняя задача в списке (приоритет повышен): {new_tasks[-1]}')
